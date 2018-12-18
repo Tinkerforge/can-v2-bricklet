8c5cdb209b8a794be5037a1ea6da447eb39e084d
Erik Fleckstein <erik@tinkerforge.com>
2018-12-18 14:51:40 +0100

    go: Change import path.

--- software/examples/go/example_loopback.go
+++ software/examples/go/example_loopback.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"fmt"
-	"tinkerforge/ipconnection"
-    "tinkerforge/can_v2_bricklet"
+	"github.com/tinkerforge/go-api-bindings/ipconnection"
+    "github.com/tinkerforge/go-api-bindings/can_v2_bricklet"
 )
 
 const ADDR string = "localhost:4223"
